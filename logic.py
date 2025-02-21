7ee59a8bbd1703698a6d189c329646aa2efdd21f
QuliyevImran <135835185+QuliyevImran@users.noreply.github.com>
2025-02-21 18:27:10 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -3,6 +3,127 @@ from datetime import datetime
 from config import DATABASE 
 import os
 import cv2
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from logic import *
+import schedule
+import threading
+import time
+from config import *
+import numpy as np
+from math import sqrt, ceil, floor
+
+def create_collage(image_paths):
+    images = []
+    for path in image_paths:
+        image = cv2.imread(path)
+        images.append(image)
+
+    num_images = len(images)
+    num_cols = floor(sqrt(num_images)) # Поиск количество картинок по горизонтали
+    num_rows = ceil(num_images/num_cols)  # Поиск количество картинок по вертикали
+    # Создание пустого коллажа
+    collage = np.zeros((num_rows * images[0].shape[0], num_cols * images[0].shape[1], 3), dtype=np.uint8)
+    # Размещение изображений на коллаже
+    for i, image in enumerate(images):
+        row = i // num_cols
+        col = i % num_cols
+        collage[row*image.shape[0]:(row+1)*image.shape[0], col*image.shape[1]:(col+1)*image.shape[1], :] = image
+    return collage
+
+
+m = DatabaseManager(DATABASE)
+info = m.get_winners_img("user_id")
+prizes = [x[0] for x in info]
+image_paths = os.listdir('img')
+image_paths = [f'img/{x}' if x in prizes else f'hidden_img/{x}' for x in image_paths]
+collage = create_collage(image_paths)
+
+cv2.imshow('Collage', collage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+bot = TeleBot(API_TOKEN)
+
+def gen_markup(id):
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton("Получить!", callback_data=id))
+    return markup
+    def get_winners_count(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id = ?', (prize_id, ))
+            return cur.fetchall()[0][0]
+
+
+
+    def get_rating(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('''
+    SELECT users.user_name, COUNT(winners.prize_id) as count_prize FROM winners
+    INNER JOIN users on users.user_id = winners.user_id
+    GROUP BY winners.user_id
+    ORDER BY count_prize
+    LIMIT 10''')
+            return cur.fetchall()
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+
+    prize_id = call.data
+    user_id = call.message.chat.id
+
+    img = manager.get_prize_img(prize_id)
+    with open(f'img/{img}', 'rb') as photo:
+        bot.send_photo(user_id, photo)
+
+
+def send_message():
+    prize_id, img = manager.get_random_prize()[:2]
+    manager.mark_prize_used(prize_id)
+    hide_img(img)
+    for user in manager.get_users():
+        with open(f'hidden_img/{img}', 'rb') as photo:
+            bot.send_photo(user, photo, reply_markup=gen_markup(id = prize_id))
+        
+
+def shedule_thread():
+    schedule.every().minute.do(send_message) # Здесь ты можешь задать периодичность отправки картинок
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.chat.id
+    if user_id in manager.get_users():
+        bot.reply_to(message, "Ты уже зарегестрирован!")
+    else:
+        manager.add_user(user_id, message.from_user.username)
+        bot.reply_to(message, """Привет! Добро пожаловать! 
+Тебя успешно зарегистрировали!
+Каждый час тебе будут приходить новые картинки и у тебя будет шанс их получить!
+Для этого нужно быстрее всех нажать на кнопку 'Получить!'
+
+Только три первых пользователя получат картинку!)""")
+        
+
+
+def polling_thread():
+    bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    manager = DatabaseManager(DATABASE)
+    manager.create_tables()
+
+    polling_thread = threading.Thread(target=polling_thread)
+    polling_shedule  = threading.Thread(target=shedule_thread)
+
+    polling_thread.start()
+    polling_shedule.start()
 
 class DatabaseManager:
     def __init__(self, database):
